72fec9e1b8d8f53012d30cd06b8132fb4184b163
Durim Kryeziu <durimkryeziu7@gmail.com>
2026-01-02 00:00:48 +0100

    Revert Application class to use public static main method

--- simple-math/src/main/java/dev/delivercraft/math/gui/Application.java
+++ simple-math/src/main/java/dev/delivercraft/math/gui/Application.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public final class Application {
+public class Application {
 
-    void main() {
-        SpringApplication.run(Application.class);
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
     }
 }
