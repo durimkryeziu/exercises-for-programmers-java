832d814603be2b8190f433bb4053c39cd7b0c3d8
Durim Kryeziu <durimkryeziu7@gmail.com>
2026-05-24 03:13:53 +0200

    refactor(paint-calculator): extract RectangularRoom and RoundRoom to separate files with permits clause

---
+++ paint-calculator/src/main/java/dev/delivercraft/paint/RectangularRoom.java
@@ -0,0 +1,17 @@
+package dev.delivercraft.paint;
+
+import java.math.BigDecimal;
+import java.util.Objects;
+
+record RectangularRoom(RoomDimension length, RoomDimension width) implements RoomDimensions {
+
+    public RectangularRoom(RoomDimension length, RoomDimension width) {
+        this.length = Objects.requireNonNull(length, "length must not be null");
+        this.width = Objects.requireNonNull(width, "width must not be null");
+    }
+
+    @Override
+    public BigDecimal area() {
+        return this.length.value().multiply(this.width.value());
+    }
+}
---
+++ paint-calculator/src/main/java/dev/delivercraft/paint/RoundRoom.java
@@ -0,0 +1,18 @@
+package dev.delivercraft.paint;
+
+import java.math.BigDecimal;
+import java.util.Objects;
+
+record RoundRoom(RoomDimension radius) implements RoomDimensions {
+
+    public RoundRoom(RoomDimension radius) {
+        this.radius = Objects.requireNonNull(radius, "radius must not be null");
+    }
+
+    @Override
+    public BigDecimal area() {
+        return this.radius.value()
+                .multiply(this.radius.value())
+                .multiply(BigDecimal.valueOf(Math.PI));
+    }
+}
